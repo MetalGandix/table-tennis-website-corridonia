1609f0dbfa73c18bec3e756b5338279d61d2bf0e
pi user <leonardo.mogianesi@studenti.unicam.it>
2022-01-25 00:30:28 +0100

    Started the project

---
+++ src/app/apps/about/About-Components/relay-on/relay-on.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relay-on',
+  templateUrl: './relay-on.component.html',
+  styleUrls: ['./relay-on.component.css']
+})
+export class RelayOnComponent implements OnInit {
+
+  relayOn = [
+    {
+      icon: 'sl-icon-envelope-open text-info-gradiant',
+      field: 'Mandaci una mail',
+      fieldText: 'leomogia@hotmail.it'
+    },
+    {
+      icon: 'sl-icon-call-end text-info-gradiant',
+      field: 'Chiamaci',
+      fieldText: 'Chiamaci al +39 3663467422'
+    },
+    {
+      icon: 'sl-icon-map text-info-gradiant',
+      field: 'Vieni da noi',
+      fieldText: 'Vieni a trovarci in loco'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/apps/blog/blog.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Blog } from './blog-type';
+import { ServiceblogService } from './blog-service.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css'],
+})
+export class BlogComponent implements OnInit {
+  blogsDetail: Blog[] = [];
+
+  constructor(
+    public service: ServiceblogService,
+    public router: Router,
+    public http: HttpClient
+  ) {
+    this.service.showEdit = false;
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  loginClick() {
+    this.router.navigate(['/login']);
+  }
+
+  newPost() {
+    this.router.navigate(['/post']);
+  }
+
+  viewDetail(id: number) {
+    this.service.detailId = id;
+
+    if (this.service.loginStatusService) this.service.showEdit = true;
+
+    this.router.navigate(['/blogDetail', id]);
+  }
+}
